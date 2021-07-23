6d87b6dc3c0b07d5cda7f427e469a2542248c570
Andrei Maiboroda <andrei@ethereum.org>
2021-07-23 12:24:35 +0200

    script for printing recent lock/unlock events

---
+++ print_recent_lock_events.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import datetime
+
+events = []
+
+f_lock_screen = open('lock_screen.log')
+for line in f_lock_screen:
+  line_split = line.strip().split()
+  # Screen unlocked on Fri Jul 16 04:30:12 PM CEST 2021
+  timestamp = datetime.datetime.strptime(" ".join(line_split[3:]), '%a %b %d %I:%M:%S %p %Z %Y')
+  events.append([timestamp, " ".join(line_split[:3])])
+
+auth_interesting = ["Lid closed", "Suspending", "Lid opened", "unlocked login keyring", "Operation 'sleep' finished"]
+
+f_auth = open('/var/log/auth.log')
+for line in f_auth:
+  line_split = line.strip().split()
+  # Jul 16 11:12:50 andrei-laptop systemd-logind[860]: Lid closed.
+  descr = " ".join(line_split[5:])
+  for interesting in auth_interesting:
+    if descr.find(interesting) != -1:
+      timestamp = datetime.datetime.strptime(" ".join(line_split[:3]), '%b %d %X')
+      #print(datetime.datetime.today().year)
+      timestamp = timestamp.replace(year = datetime.datetime.today().year)
+      events.append([timestamp, descr])
+
+events.sort()
+
+for event in events:
+  print(event[0], event[1])
