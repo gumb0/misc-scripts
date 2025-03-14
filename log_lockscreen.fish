c07187eac35f5ab5bdc4e428347bdd9a3d07bf7a
Andrei Maiboroda <andrei@ethereum.org>
2025-03-14 15:05:50 +0100

    Improve log_lockscreen.fish (include wifi name)

--- log_lockscreen.fish
+++ log_lockscreen.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 while true
   read X; or exit 1
+  set WIFI_NAME (iwgetid -r)
   if echo "$X" | grep "boolean true" &>/dev/null
-    echo "Screen   locked on "(date)
+    echo "Screen   locked on "(date)" Wi-Fi:" $WIFI_NAME
   else if echo "$X" | grep "boolean false" &>/dev/null
-    echo "Screen unlocked on "(date)
+    echo "Screen unlocked on "(date)" Wi-Fi:" $WIFI_NAME
   end
-end >$HOME/lock_screen.log
+end >>$HOME/lock_screen.log
