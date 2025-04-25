2b3495b7e33c7c51b9161872e7e8fc6bf14ff5f4
Andrei Maiboroda <andrei@ethereum.org>
2025-04-25 17:08:25 +0200

    Output timestamps in ISO8601 in log_lockscreen.fish

--- log_lockscreen.fish
+++ log_lockscreen.fish
@@ -3,8 +3,8 @@ while true
   read X; or exit 1
   set WIFI_NAME (iwgetid -r)
   if echo "$X" | grep "boolean true" &>/dev/null
-    echo "Screen   locked on "(date)" Wi-Fi:" $WIFI_NAME
+    echo (date +"%Y-%m-%dT%H:%M:%S%:z") "Screen   locked. Wi-Fi:" $WIFI_NAME
   else if echo "$X" | grep "boolean false" &>/dev/null
-    echo "Screen unlocked on "(date)" Wi-Fi:" $WIFI_NAME
+    echo (date +"%Y-%m-%dT%H:%M:%S%:z") "Screen unlocked. Wi-Fi:" $WIFI_NAME
   end
 end >>$HOME/lock_screen.log
--- print_recent_lock_events.py
+++ print_recent_lock_events.py
@@ -6,24 +6,20 @@ events = []
 f_lock_screen = open('lock_screen.log')
 for line in f_lock_screen:
   line_split = line.strip().split()
-  # TODO %Z for matches the current timezone only, so any change, e.g. daylight savings breaks this for older records
-  # TODO potentially use dateutil.parser instead
-  # Screen unlocked on Fri Jul 16 04:30:12 PM CEST 2021
-  timestamp = datetime.datetime.strptime(" ".join(line_split[3:10]), '%a %b %d %I:%M:%S %p %Z %Y')
-  events.append([timestamp, " ".join(line_split[:3] + line_split[10:])])
+  # 2025-04-11T16:25:50+02:00 Screen unlocked. Wi-Fi: xxx
+  timestamp = datetime.datetime.fromisoformat(line_split[0])
+  events.append([timestamp, " ".join(line_split[1:])])
 
 auth_interesting = ["Lid closed", "Suspending", "Lid opened", "unlocked login keyring", "Operation 'sleep' finished"]
 
 f_auth = open('/var/log/auth.log')
 for line in f_auth:
   line_split = line.strip().split()
-  # Jul 16 11:12:50 andrei-laptop systemd-logind[860]: Lid closed.
-  descr = " ".join(line_split[5:])
+  # 2025-04-08T11:19:35.410373+02:00 andrei-laptop systemd-logind[1597]: Lid opened
+  descr = " ".join(line_split[3:])
   for interesting in auth_interesting:
     if descr.find(interesting) != -1:
-      timestamp = datetime.datetime.strptime(" ".join(line_split[:3]), '%b %d %X')
-      #print(datetime.datetime.today().year)
-      timestamp = timestamp.replace(year = datetime.datetime.today().year)
+      timestamp = datetime.datetime.fromisoformat(line_split[0])
       events.append([timestamp, descr])
 
 events.sort()
